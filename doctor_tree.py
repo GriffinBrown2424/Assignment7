b4a652730f784838fe4295106afdce1b01c80fdb
Riley Mclaughlin <rileymclaughlin@Rileys-MacBook-Pro-4.local>
2026-02-08 19:59:13 -0500

    Debugged final output issues

--- doctor_tree.py
+++ doctor_tree.py
@@ -1,12 +1,55 @@
 class DoctorNode:
-    pass # Delete this line and implement the DoctorNode class
-
+    """
+    Represents a doctor in the reporting hierarchy.
+    """
+    def __init__(self, name):
+        self.name = name
+        self.left = None
+        self.right = None
 
 
 class DoctorTree:
-    pass # Delete this line and implement the DoctorTree class
+    """
+    Manages the binary tree structure of doctor reports.
+    """
+    def __init__(self):
+        self.root = None
 
+    def insert(self, parent_name, doctor_name, side, current_node=None):
+        if self.root is None:
+            return False
 
+        if current_node is None:
+            current_node = self.root
 
+        if current_node.name == parent_name:
+            if side == "left" and current_node.left is None:
+                current_node.left = DoctorNode(doctor_name)
+                return True
+            if side == "right" and current_node.right is None:
+                current_node.right = DoctorNode(doctor_name)
+                return True
+            return False
 
-# Test your DoctorTree and DoctorNode classes here
+        found = False
+        if current_node.left:
+            found = self.insert(parent_name, doctor_name, side, current_node.left)
+        if not found and current_node.right:
+            found = self.insert(parent_name, doctor_name, side, current_node.right)
+
+        return found
+
+    def preorder(self, node):
+        if node is None:
+            return []
+        return [node.name] + self.preorder(node.left) + self.preorder(node.right)
+
+    def inorder(self, node):
+        if node is None:
+            return []
+        return self.inorder(node.left) + [node.name] + self.inorder(node.right)
+
+    def postorder(self, node):
+        if node is None:
+            return []
+        return self.postorder(node.left) + self.postorder(node.right) + [node.name]
--- emergency_queue.py
+++ emergency_queue.py
@@ -1,12 +1,67 @@
 class Patient:
-    pass # Delete this line and implement the Patient class
-
+    """
+    Represents a patient waiting for emergency care.
+    """
+    def __init__(self, name, urgency):
+        self.name = name
+        self.urgency = urgency
 
 
 class MinHeap:
-    pass # Delete this line and implement the MinHeap class
+    """
+    MinHeap for managing patients by urgency.
+    """
+    def __init__(self):
+        self.data = []
 
+    def heapify_up(self, index):
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.data[index].urgency < self.data[parent].urgency:
+                self.data[index], self.data[parent] = self.data[parent], self.data[index]
+                index = parent
+            else:
+                break
 
+    def heapify_down(self, index):
+        size = len(self.data)
+        while True:
+            left = 2 * index + 1
+            right = 2 * index + 2
+            smallest = index
 
+            if left < size and self.data[left].urgency < self.data[smallest].urgency:
+                smallest = left
+            if right < size and self.data[right].urgency < self.data[smallest].urgency:
+                smallest = right
 
-# Test your MinHeap class here including edge cases
+            if smallest != index:
+                self.data[index], self.data[smallest] = self.data[smallest], self.data[index]
+                index = smallest
+            else:
+                break
+
+    def insert(self, patient):
+        self.data.append(patient)
+        self.heapify_up(len(self.data) - 1)
+
+    def remove_min(self):
+        if not self.data:
+            return None
+
+        if len(self.data) == 1:
+            return self.data.pop()
+
+        root = self.data[0]
+        self.data[0] = self.data.pop()
+        self.heapify_down(0)
+        return root
+
+    def peek(self):
+        if not self.data:
+            return None
+        return self.data[0]
+
+    def print_heap(self):
+        for patient in self.data:
+            print(f"{patient.name}: {patient.urgency}")
